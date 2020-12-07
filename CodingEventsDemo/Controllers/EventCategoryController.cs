56f5cf5a1b9ad51612ae26723051c66670badb5f
Haley <haleylaneart@gmail.com>
2020-12-06 18:49:56 -0600

    * Startup.cs: * Event.cs: * EventData.cs: * EventCategory.cs: * EventsController.cs: * Index.cshtml: * EventCategoryController.cs: * 20201207002733_SecondMigration.cs: * 20201207002733_SecondMigration.Designer.cs:
    * CodingEventsDemo.csproj: Exercises 19.4.5 completed so Exercises
      completed

---
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext cat;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            cat = dbContext;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<EventCategory> categories = cat.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
