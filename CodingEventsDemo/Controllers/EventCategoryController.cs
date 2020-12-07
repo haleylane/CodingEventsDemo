bfae9588ad22ed5bca5311fcefe856b854949bc6
Haley <haleylaneart@gmail.com>
2020-12-07 11:23:50 -0600

    19 studio complete

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEventsDemo.Controllers
@@ -23,5 +24,32 @@ namespace CodingEventsDemo.Controllers
             List<EventCategory> categories = cat.Categories.ToList();
             return View(categories);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+            return View(addEventCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newCategory = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                };
+
+                cat.Categories.Add(newCategory);
+                cat.SaveChanges();
+
+                return Redirect("/EventCategory");
+            }
+
+            return View("Create", addEventCategoryViewModel);
+        }
+        
     }
 }
